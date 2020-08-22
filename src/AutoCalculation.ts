9ca9fa2845aeda7d18fb351426f21d06695534da
coccoto <soprano.lune@gmail.com>
2020-08-22 23:54:16 +0900

    2020-08-22 23:54

--- src/AutoCalculation.ts
+++ src/AutoCalculation.ts
@@ -23,8 +23,9 @@ export default class AutoCalculation {
         selected.setValues(values)
     }
 
-    private calculate(tables: {[name: string]: string[][]}, baranceValue: number): {[name: string]: string[][]} {
+    private calculate(tables: {[name: string]: string[][]}, baranceValue: number, addFlag: boolean): {[name: string]: string[][]} {
 
+        this.calculator.switchAdd(addFlag)
         return this.calculator.main(tables, baranceValue)
     }
 
@@ -62,17 +63,25 @@ export default class AutoCalculation {
 
     public main(rowBalance: number = 2, columnBalance: number = 5): void {
 
+        // 残高セルから残高を取得する。
         let cellBarance: Spreadsheet.Range = this.sheet.getRange(rowBalance, columnBalance)
         let baranceValue: string = cellBarance.getValue()
 
+        // 残高セルが空欄の時、全ての処理を終了する。
         if (baranceValue === '') {
             return
         }
+        // テーブルの縦幅を取得する。
         const tableLength: number = this.tableLength()
+        // 残高セルから下の情報をテーブルの縦幅分取得する。
         const selected: Spreadsheet.Range = this.select(rowBalance, columnBalance, tableLength)
+        // 計算処理に用いる情報を二次元配列で取得する。
         const tables: {[name: string]: string[][]} = this.tableRegister(selected)
+        // 計算処理を決定する。
+        const cellAddSwitch: Spreadsheet.Range = this.sheet.getRange(rowBalance, columnBalance - 1)
+        const addFlag: boolean = cellAddSwitch.getValue()
 
-        const result: {[name: string]: string[][]} = this.calculate(tables, Number(baranceValue))
+        const result: {[name: string]: string[][]} = this.calculate(tables, Number(baranceValue), addFlag)
         this.refresh(selected, result)
 
         this.main(rowBalance, columnBalance + 3)
--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,25 +1,54 @@
 export default class Calculator {
 
+    private addFlag: boolean
+
+    public constructor() {
+
+        this.addFlag = false
+    }
+
+    /**
+     * 背景色が白の時は残高をマイナス、白以外の時はプラスする。
+     * addFlag が true の時は上記と逆処理を実行する。
+     */
     private push(values: string[], colors: string[], baranceValue: number): string {
 
         if (colors[0] === '#ffffff') {
-            return values[1] = String(baranceValue - Number(values[0]))
+            if (this.addFlag) {
+                return values[1] = String(baranceValue + Number(values[0]))
+            } else {
+                return values[1] = String(baranceValue - Number(values[0]))
+            }
         } else {
-            return values[1] = String(baranceValue + Number(values[0]))
+            if (this.addFlag) {
+                return values[1] = String(baranceValue - Number(values[0]))
+            } else {
+                return values[1] = String(baranceValue + Number(values[0]))
+            }
         }
     }
 
     private calculate(tables: {[name: string]: string[][]}, baranceValue: number): {[name: string]: string[][]} {
 
         for (let index in tables.values) {
+            // テーブルに値が存在していれば計算処理を実行する。
             if (tables.values[index][0] !== '') {
                 tables.values[index][1] = this.push(tables.values[index], tables.colors[index], baranceValue)
             }
+            //　テーブルに値が存在していれば残高を更新する。
             if (tables.values[index][1] !== '') {
                 baranceValue = Number(tables.values[index][1])
             }
         }
         return tables
+    }
+
+    /**
+     * 計算の処理方法を決定する。
+     */
+    public switchAdd(addFlag: boolean): void {
+
+        this.addFlag = addFlag
     }
 
     public main(tables: {[name: string]: string[][]}, baranceValue: number): {[name: string]: string[][]} {
